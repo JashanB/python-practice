58e897d40f0870f206a64f1696cd163f9380e9e9
JashanB <jashan_brar50@hotmail.com>
2020-06-15 19:15:58 -0400

    pdf to txt successful

--- pdf.py
+++ pdf.py
@@ -4,6 +4,6 @@ pdfFileObject = open("eg.pdf", 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
 pageObject = pdfReader.getPage(0)
 text = pageObject.extractText()
-file1 = open("/Users/JD/lighthouse/python/example.txt")
+file1 = open("/Users/JD/lighthouse/python/example.txt", "a")
 file1.writelines(text)
 file1.close
