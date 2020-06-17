e99e884cf87db7a1144cd4fe28e6c6f1183e06ef
JashanB <jashan_brar50@hotmail.com>
2020-06-16 20:05:33 -0400

    further testing

--- pdf.py
+++ pdf.py
@@ -1,9 +1,9 @@
 import PyPDF2
 from PyPDF2 import PdfFileWriter, PdfFileReader
-pdfFileObject = open("test.pdf", 'rb')
+pdfFileObject = open("test3.pdf", 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
 pageObject = pdfReader.getPage(0)
 text = pageObject.extractText()
-file1 = open("/Users/JD/lighthouse/python/test1.txt", "a")
+file1 = open("/Users/JD/lighthouse/python/test2.txt", "a")
 file1.writelines(text)
 file1.close
